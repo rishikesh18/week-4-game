95c1c307a2ff163c8d5512dfacdacffe1fa344be
rishikesh18 <rishi.neupane@gmail.com>
2018-03-20 18:20:27 -0400

    homework correction

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,10 +4,7 @@ var userTotal, random, num1, num2, num3, num4;
 var losses = 0;
 var wins = 0;
 
-$(document).ready(function(){
-    initializeValue();
-    
-});
+
 
 function initializeValue() {
      userTotal = 0;
@@ -21,10 +18,8 @@ function initializeValue() {
      
 }
 
-function reset() {
-    initializeValue();
-}
 
+    
 function addScore(num) {
     userTotal = userTotal + num;
     $("#score").text(userTotal);
@@ -32,13 +27,13 @@ function addScore(num) {
         $('.won').stop().fadeIn(400).delay(2000).fadeOut(400); 
         wins++;
         $("#win").text("Wins: " + wins);
-        reset();
+        initializeValue();
     }
         else if (userTotal > random) {
         $('.lost').stop().fadeIn(400).delay(2000).fadeOut(400); 
         losses++;
         $("#loss").text("Losses: " + losses);
-        reset();
+        initializeValue();
     }
             else {
 
@@ -68,53 +63,13 @@ $("#crys2").click(function(){
        
  });
 
-
-
-
-
-
-/*
-    $("#crys2").click(num2);
-    $("#crys3").click(num3);
-    $("#crys4").click(num44);  */
-  
-   
-
-
-
- /*   var userTotal= 0
-    var wins= 0;
-    var losses = 0;
-
- /*   $("#crys1").click(function(){
-        userTotal = userTotal + num1
-    });
-    $("#crys2").click(function(){
-        userTotal = userTotal + num2
-    });
-    $("#crys3").click(function(){
-        userTotal = userTotal + num3
-    });
-    $("#crys4").click(function(){
-        userTotal = userTotal + num4 
-    });
+ $(document).ready(function(){
+    initializeValue();
     
+});
 
-   
-   
-  /*  $("#win").text("Wins: " + wins);
-    $("#loss").text("Losses: " + losses);
 
-  /*  function reset(){
-        Random=Math.floor(Math.random()*((90-60)+1) + 60);
-        console.log(Random)
-        $("#randomNumber").text(Random);
-        num1=Math.floor(Math.random()*((15-1)+1) + 1);
-        num2=Math.floor(Math.random()*((15-1)+1) + 1);
-        num3=Math.floor(Math.random()*((15-1)+1) + 1);
-        num4=Math.floor(Math.random()*((15-1)+1) + 1);
-        userTotal= 0;
-        $("#score").text(userTotal);
-        } 
 
-    $("#crys1").on("click") */
+
+
+
